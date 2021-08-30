43b88f7d29ca4d5e1ad76207f6f128654d7b62e0
jamiejohnsonkc <jamiejohnsonkc@gmail.com>
2021-08-30 15:33:00 -0500

    updated file structure and related links

---
+++ dom_exercises__events/js-file__events.js
@@ -0,0 +1,32 @@
+// const textBox = document.querySelector(".content");
+// const redP = document.createElement("p");
+// redP.textContent = "Hey I'm Red";
+// redP.classList.add("redP");
+// redP.style.color = "red";
+// textBox.appendChild(redP);
+
+// const blueH3 = document.createElement("h3");
+// blueH3.textContent = "I'm a blue h3!";
+// blueH3.classList.add("blueH3");
+// blueH3.style.color = "blue";
+// textBox.appendChild(blueH3);
+
+// const newDiv = document.querySelector(".content");
+// const newDivDiv = document.createElement("div");
+// newDivDiv.classList.add("newDivDiv");
+// newDivDiv.style = {
+//   border: "black",
+//   backgroundColor: "pink",
+// };
+
+// const newH1 = document.querySelector("newDivDiv");
+// const newH1H1 = document.createElement("h1");
+// newH1H1.textContent = "I'm in a div";
+// newDiv.appendChild(newH1H1);
+
+// const newP = document.querySelector("newDivDiv");
+// const newPP = document.createElement("p");
+// newPP.textContent = "ME TOO!";
+// newDiv.appendChild(newPP);
+
+// newDiv.appendChild(newDivDiv);
